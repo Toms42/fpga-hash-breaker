4bc3ef3f252933ad96e4dffa4459e1ec51e1be65
Toms42 <tomscherlis@gmail.com>
2016-12-16 23:55:34 -0500

    cleaned up old files

--- design files/memory/md5krom_inst.v
+++
@@ -1,5 +0,0 @@
-md5krom	md5krom_inst (
-	.address ( address_sig ),
-	.clock ( clock_sig ),
-	.q ( q_sig )
-	);
--- design files/memory/md5srom_inst.v
+++
@@ -1,5 +0,0 @@
-md5srom	md5srom_inst (
-	.address ( address_sig ),
-	.clock ( clock_sig ),
-	.q ( q_sig )
-	);

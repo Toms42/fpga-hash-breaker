fd9b64ffc80f76da7ac945223baec0c1cbdb3680
Toms42 <tomscherlis@gmail.com>
2017-01-30 14:13:22 -0500

    testbench complete, vvp fails...

--- design files/hash_testbench.v
+++ design files/hash_testbench.v
@@ -1,17 +1,40 @@
 module testbench();
 
 reg clk = 0;
-reg[447:0] message = "testeroni";
+reg[447:0] message = "test";
+wire[63:0] len;
 
+wire[7:0] state;
 wire[127:0] hash;
 wire [511:0] message_out;
 
 md5core hasher(
 	.message(message),
-	.length(64'd72),
+	.length(len),
 	.clk(clk),
 	.hash(hash),
 	.message_out(message_out)
+);
+
+reg initiate=0;
+reg program=0;
+
+parameter[8*127-1:0] charlist_example="abcdefghijklmnopqrstuvwxyz1234567890";
+reg[25*8*127-1:0] prg_format={25{charlist_example}};
+
+
+generator phrase_gen(
+	.clk(clk),
+	.initiate(initiate),
+	.program(program),
+	.prg_format(prg_format),
+	.prg_num_characters(8'd36),
+	.prg_len(8'd5),
+	.prg_h_goal("test"),
+	.h_res(hash),
+	.m(m),
+	.m_len(len),
+	.state(state)
 );
 
 reg[7:0] count=0;
@@ -20,14 +43,21 @@ always begin
 	#1 clk = !clk;
 	if(clk==1)
 	begin
-		if(count==66)
+		if(count==1)
 		begin
-			$display("hash=%x for message=%x",hash,message_out);
-
-			for(count2=0;count2<64;count2=count2+1)
-			begin
-				//$display("\t%d | %x",count2,message_out[(512-8-count2*8)+:8]);
-			end
+			program<=1;
+		end
+		else if(count==3)
+		begin
+			program<=0;
+		end
+		else if(count==5)
+		begin
+			initiate<=1;
+		end
+		else if(count==6)
+		begin
+			initiate<=0;
 		end
 	count<=count+1;
 	end
--- design files/phrase_generator.v
+++ design files/phrase_generator.v
@@ -15,12 +15,12 @@ module generator(
 	input wire[7:0] prg_len,
 	input wire[127:0] prg_h_goal,
 
-	input wire[127:0] h_success,
-	input wire[511:0] m_success,
+	input wire[127:0] h_res,
+	input wire[511:0] m_res,
 
-	output reg[447:0] m,
+	output wire[447:0] m,
 	output reg[63:0] m_len,
-	output reg[7:0] state
+	output reg[7:0] state=`STATE_IDLE
 	);
 
 reg[25*8*127-1:0] format;
@@ -29,7 +29,7 @@ reg[25*8-1:0] lengths;
 reg[7:0] len = 0;
 reg[7:0] num_characters = 0;
 
-reg[7:0] state=`STATE_IDLE;
+reg[127:0] h_goal=0;
 
 wire count;
 assign count = state==`STATE_START ? clk : 0;
@@ -43,69 +43,74 @@ generate
 	genvar i;
 		for(i=0;i<25;i=i+1)
 		begin : generate_char_generators
-			character_counter(
+			character_counter char_gen_i(
 				.count(count),
 				.carry_in(carry_array[i]),
 				.carry_out(carry_array[i+1]),
-				.prg_num_chars(lengths[i*8+:8]),
+				.prg_numchars(lengths[i*8+:8]),
 				.prg_charlist(format[i*8*127+:8*127]),
 				.enable(enable_array[i]),
 				.program(programming),
-				.char(m[8*i+:8])
+				.char(m[8*i +: 8])
 			);
 		end
 
 endgenerate
 
-reg[4:0] i=0;
+reg[4:0] index=0;
+reg[4:0] index2=0;
 
 always @(posedge clk)
 begin
-	case state
+	case (state)
+
+	`STATE_PROGRAMMING: 
 	begin
-
-		`STATE_PROGRAMMING: 
+		format<=prg_format;
+		len<=prg_len;
+		num_characters<=prg_num_characters;
+		h_goal<=prg_h_goal;
+		m_len<=prg_len;
+		for(index2=0;index2<25;index2+=1)
 		begin
-			format<=prg_format;
-			len<=prg_len;
-			num_characters<=prg_num_characters;
-
-			enable_array<=0;
-
-			for(i=0;i<num_characters;i++)
-			begin
-				enable_array[i+1]=1'b1
-			end
-
-			state<=`STATE_IDLE;
-
+			enable_array[index2]<=0;
 		end
 
-		`STATE_IDLE:
+		for(index=0;index<num_characters;index++)
 		begin
-			if(initiate==1'b1)
-			begin
-				state<=`STATE_RUNNING;
-			end
+			enable_array[index+1]=1'b1;
 		end
 
-		`STATE_START:
+		state<=`STATE_IDLE;	
+	end
+
+	`STATE_IDLE:
+	begin
+		if(initiate==1'b1)
 		begin
-			state <= `STATE_RUNNING;
+			state<=`STATE_RUNNING;
 		end
-
-		`STATE_RUNNING:
+		else if(program==1'b1)
 		begin
-			if(h_goal==h_res) //check if already cracked
-			begin
-				state=`STATE_SUCCESS;
-				$display("success! | %x (%x)",m_res,h_res);
-			end
-
-			else begin
-				m<={}
-			end
+			state<=`STATE_PROGRAMMING;
 		end
 	end
-	
-end
+
+	`STATE_START:
+	begin
+		state <= `STATE_RUNNING;
+	end
+
+	`STATE_RUNNING:
+	begin
+		if(h_goal==h_res) //check if already cracked
+		begin
+			state=`STATE_SUCCESS;
+			$display("success! | %x (%x)",m_res,h_res);
+		end
+	end
+
+	endcase
+
+end	
+endmodule
